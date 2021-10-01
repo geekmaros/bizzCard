d709cdf64e48ce5bc7df077d4182e31f19484c9e
geekmaros <razicruz@gmail.com>
2021-10-01 20:30:26 +0100

    init files

---
+++ layouts/dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+<section class='h-screen font-serif overflow-x-hidden overflow-y-hidden relative  grid grid-cols-12'>
+  <SideBar class='col-span-2'/>
+  <Nuxt keep-alive class='col-span-10 overflow-y-scroll'/>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/admin/card/create.vue
@@ -0,0 +1,16 @@
+<template>
+<section>
+<NuxtChild/>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'Create',
+  layout: 'dashboard'
+}
+</script>
+
+<style scoped>
+
+</style>
